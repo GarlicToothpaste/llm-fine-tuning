3b9c7861a0930d0256af3d7f8dd32cf2d221db0c
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-25 22:32:36 +0800

    Saved the trained model

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-# import pandas as pd
+import pandas as pd
 
-# splits = {'train': 'synthetic_text_to_sql_train.snappy.parquet', 'test': 'synthetic_text_to_sql_test.snappy.parquet'}
-# df = pd.read_parquet("hf://datasets/gretelai/synthetic_text_to_sql/" + splits["train"])
-
-
-from unsloth import FastLanguageModel
-import torch
-
-max_seq_length = 2048
-dtype = None
-load_in4bit
+splits = {'train': 'synthetic_text_to_sql_train.snappy.parquet', 'test': 'synthetic_text_to_sql_test.snappy.parquet'}
+df = pd.read_parquet("hf://datasets/gretelai/synthetic_text_to_sql/" + splits["train"])
