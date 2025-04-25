44502c2976c3264512dbaef175118fd3a40cee5b
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-25 20:59:58 +0800

    added more to gitignore

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-import pandas as pd
+# import pandas as pd
 
-splits = {'train': 'synthetic_text_to_sql_train.snappy.parquet', 'test': 'synthetic_text_to_sql_test.snappy.parquet'}
-df = pd.read_parquet("hf://datasets/gretelai/synthetic_text_to_sql/" + splits["train"])
+# splits = {'train': 'synthetic_text_to_sql_train.snappy.parquet', 'test': 'synthetic_text_to_sql_test.snappy.parquet'}
+# df = pd.read_parquet("hf://datasets/gretelai/synthetic_text_to_sql/" + splits["train"])
+
+
+from unsloth import FastLanguageModel
+import torch
+
+max_seq_length = 2048
+dtype = None
+load_in4bit
