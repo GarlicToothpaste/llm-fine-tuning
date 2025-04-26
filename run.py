bed9ea2837e19b74dd53edb70029383012ff9847
GarlicToothpaste <adrianpdanao@gmail.com>
2025-04-26 08:37:12 +0800

    Cleaned up unused files

--- run.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-splits = {'train': 'synthetic_text_to_sql_train.snappy.parquet', 'test': 'synthetic_text_to_sql_test.snappy.parquet'}
-df = pd.read_parquet("hf://datasets/gretelai/synthetic_text_to_sql/" + splits["train"])
